a4305a73460266c63eb4d2c49fb139d0a469ee49
Koray <koray_simsek05@hotmail.com>
2026-01-27 19:48:15 +0300

    test

--- PortfolyoSitesi/wwwroot/html/assets/vendor/jquery-validation/dist/jquery.validate.js
+++ PortfolyoSitesi/wwwroot/html/assets/vendor/jquery-validation/dist/jquery.validate.js
@@ -362,9 +362,9 @@ $.extend( $.validator, {
 	},
 
 	messages: {
-		required: "This field is required.",
+		required: "Bu alan boş bırakılamaz.",
 		remote: "Please fix this field.",
-		email: "Please enter a valid email address.",
+		email: "Lütfen doğru bir e-posta adresi girin.",
 		url: "Please enter a valid URL.",
 		date: "Please enter a valid date.",
 		dateISO: "Please enter a valid date (ISO).",
